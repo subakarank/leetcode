3a52257aced6122c4444bdd47b6fd1ca394da597
Subakaran <subakaranmca@gmail.com>
2024-05-23 11:01:25 +0100

    two sum updated

--- two_sum.py
+++ two_sum.py
@@ -1,10 +1,23 @@
-#Two Sum
+# Two Sum
 '''
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
 You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
 '''
 
 from typing import List
@@ -19,13 +32,11 @@ def brute_force_two_sum(lst: List, target: int):
 def two_sum(lst: List, target: int):
     list_to_dict = {}
     for key, value in enumerate(lst):
+        complement = target - value
+        if complement in list_to_dict:
+            return [list_to_dict[complement], key]
         list_to_dict[value] = key
-    for i in range(len(lst)):
-        complement = target - lst[i] 
-        if complement in list_to_dict and i != list_to_dict[complement]:
-            return [i, list_to_dict[complement]]
     return -1
-
 
 print("""Brute-force solution""")
 print(f"list[2,5,7,4] and target 11: {brute_force_two_sum([2,5,7,4], 11 )}")

3a97d527b8c0eafdb4b3da1974165cd433fa057b
Subakaran <subakaranmca@gmail.com>
2024-06-01 16:21:35 +0100

    Added moving average

--- movin_average.py
+++ movin_average.py
@@ -1,3 +1,13 @@
+'''
+https://leetcode.com/problems/moving-average-from-data-stream/description/
+Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
+
+Implement the MovingAverage class:
+
+MovingAverage(int size) Initializes the object with the size of the window size.
+double next(int val) Returns the moving average of the last size values of the stream.
+'''
+
 class MovingAverage():
     def __init__(self, size: int):
         self.size = size
